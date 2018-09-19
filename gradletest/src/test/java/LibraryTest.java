e6b2c25f6441418bbdd978b45ea5658b36c79f08
dvo <Dominik.Vogt@css.de>
2018-09-19 16:06:14 +0200

    Anpassungen Besipiel

---
+++ gradletest/src/test/java/LibraryTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+/*
+ * Copyright (c) 2005-2018 CSS AG.
+ * All Rights Reserved.
+ *
+ * This file may not be reproduced in any form or by any means
+ * (graphic, electronic or mechanical) without written permission
+ * from CSS AG.
+ *
+ * $HeadURL$
+ * $Rev$
+ * $Date$
+ * $Author$
+ */
+
+public class LibraryTest {
+
+
+	/**
+	 * Test method for {@link Library#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() throws Exception {
+		Library.main(null);
+	}
+}
